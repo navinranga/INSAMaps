e1ed5d1ca2110d517ac79c6ac56bc5746d1eaa60
navinranga <ranganavinjacques@gmail.com>
2022-03-23 22:56:26 +0100

    logo

---
+++ fenetre.java
@@ -0,0 +1,51 @@
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class fenetre extends JFrame implements MouseListener {
+    JFrame F;
+    public final static int HT =1073;
+    public final static int LG =762;
+
+    public fenetre() {
+        JFrame F = new JFrame("INSA Maps");
+        F.setSize(HT,LG);
+        F.setVisible(true);
+        F.addWindowListener(new gestionFenetre());
+        ImageIcon icone = new ImageIcon("INSA_Maps2.png");
+        JLabel image = new JLabel(icone);
+        F.add(image);
+        F.setVisible(true);
+        F.setResizable(false);
+        F.addMouseListener(this);
+
+        Image icon = Toolkit.getDefaultToolkit().getImage("logo.png");    
+        F.setIconImage(icon);  
+        
+    }
+
+    public void mouseReleased(MouseEvent e){}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+	public void mousePressed(MouseEvent e){}
+
+	public void mouseClicked(MouseEvent e){
+		System.out.println("Vous avez clique avec la souris");
+		String s = "("+e.getX()+","+e.getY()+")";
+		System.out.println(s);
+	}
+}
+
+class gestionFenetre extends WindowAdapter{
+    public void windowClosing(WindowEvent e){
+        System.exit(0);
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+		fenetre mafenetre = new fenetre();
+	}
+}
+
+
